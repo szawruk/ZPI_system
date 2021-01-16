f970b6f59458c3331bcf5bfdda84fc6f9098aaa0
Agooon <agonplp@gmail.com>
2021-01-16 23:19:42 +0100

    Added database ConnectionString, ZPIContext.cs and Topic.cs
    Topic.cs is in final form.
    ZPIContext.cs have basic setup with constructor, and DbSet<Topic> Topics as property

---
+++ ZPI_backend/ZPI_Database/DataAccess/ZPIContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZPI_Database.Models;
+
+namespace ZPI_Database.DataAccess
+{
+    public class ZPIContext: DbContext
+    {
+        public ZPIContext(DbContextOptions<ZPIContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Topic> Topics { get; set; }
+    }
+}
---
+++ ZPI_backend/ZPI_Database/Models/Topic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPI_Database.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "VARCHAR(255)")]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "VARCHAR(255)")]
+        [Required]
+        public string Description { get; set; }
+    }
+}
